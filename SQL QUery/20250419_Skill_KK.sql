373ffabb995517b29e40be824329ce606993c1fa
Karthi <karthimyle1997@gmail.com>
2025-05-23 11:10:12 +0530

    SQL push

---
+++ SQL QUery/20250419_Skill_KK.sql
@@ -0,0 +1,108 @@
+Insert Into KMM.Skill(TItle,Isactive,Created_By,Created_At)
+Values
+('Syntax',1,'KK',Getdate()),
+('HTML',1,'KK',Getdate()),
+('CSS',1,'KK',Getdate()),
+('SCSS',1,'KK',Getdate()),
+('SASS',1,'KK',Getdate()),
+
+('Javascript',1,'KK',Getdate()),
+('Event Handling',1,'KK',Getdate()),
+('Forms Handling',1,'KK',Getdate()),----Is different for all the UI 
+('UI Performance Optimization',1,'KK',Getdate()),
+('LeftCycle',1,'KK',Getdate()),
+('HTTP Requests',1,'KK',Getdate()),
+
+('Responsive Design',1,'KK',Getdate()),
+('Node',1,'KK',Getdate()),
+('Next.js',1,'KK',Getdate()),
+
+('TypeScipt',1,'KK',Getdate()),
+('Angular UI Optimization',1,'KK',Getdate()),
+('Angular Component',1,'KK',Getdate()),
+('Angular Dependency Injection',1,'KK',Getdate()),
+('Angular Forms ',1,'KK',Getdate()),
+('RxJS',1,'KK',Getdate()),
+('Observables',1,'KK',Getdate()),
+('NgModules & Services',1,'KK',Getdate()),
+
+('Redux',1,'KK',Getdate()),
+('CLI',1,'KK',Getdate()),
+('Version Control',1,'KK',Getdate()),
+('GIT',1,'KK',Getdate()),
+('React Router',1,'KK',Getdate()),
+('React Query ',1,'KK',Getdate()),
+('Custom Hooks',1,'KK',Getdate()),
+
+
+
+('Routing',1,'KK',Getdate()),
+('API Integration',1,'KK',Getdate()),
+('React Component',1,'KK',Getdate()),
+('Hooks',1,'KK',Getdate()),
+('JSX',1,'KK',Getdate()),
+('Context API',1,'KK',Getdate()),
+
+
+---Back End
+---.Net a
+('Design Pattern',1,'KK',Getdate()),
+('Micro Services',1,'KK',Getdate()),
+('.net Dependency Injection',1,'KK',Getdate()),
+('.net Event Handling',1,'KK',Getdate()),
+
+('MVC architecture',1,'KK',Getdate()),
+('Middleware and Routing',1,'KK',Getdate()),
+('Tag Helpers & View Components',1,'KK',Getdate()),
+('LNIQ',1,'KK',Getdate()),
+('Migrations',1,'KK',Getdate()),
+('CLR',1,'KK',Getdate()),
+('.net Exception handling',1,'KK',Getdate()),
+('C# Language',1,'KK',Getdate()),
+('Assemblies',1,'KK',Getdate()),
+('NuGet Packages',1,'KK',Getdate()),
+('RESTful',1,'KK',Getdate()),
+('SignalR',1,'KK',Getdate()),
+('API Gateway',1,'KK',Getdate()),
+('Service Discovery',1,'KK',Getdate()),
+('Asynchronous Programming',1,'KK',Getdate()),
+
+--Node.js
+('Global objects ',1,'KK',Getdate()),
+('Core modules',1,'KK',Getdate()),
+('HTTP',1,'KK',Getdate()),
+('NPM',1,'KK',Getdate()),
+('Express.js Framework',1,'KK',Getdate()),
+('Templating Engines',1,'KK',Getdate()),
+('Node Middleware and Logging',1,'KK',Getdate()),
+('Authentication & Authorization',1,'KK',Getdate()),
+('Event-Driven Architecture',1,'KK',Getdate()),
+('Profiling and memory leak detection',1,'KK',Getdate()),
+('Event loop understanding',1,'KK',Getdate()),
+('Clustering and load balancing',1,'KK',Getdate()),
+
+('RESTful',1,'KK',Getdate()),
+
+
+
+
+---RUST
+('Ownership System',1,'KK',Getdate()),
+('Rust Event Handling',1,'KK',Getdate()),
+('Pattern Matching',1,'KK',Getdate()),
+('Crates and Modules',1,'KK',Getdate()),
+('Dependency management',1,'KK',Getdate()),
+('Collections',1,'KK',Getdate()),
+('Traits & Generics',1,'KK',Getdate()),
+('Concurrency ',1,'KK',Getdate()),
+('Unsafe Rust',1,'KK',Getdate()),
+('Macros',1,'KK',Getdate()),
+('Advanced Traits',1,'KK',Getdate()),
+('Tooling',1,'KK',Getdate()),
+('SignalR',1,'KK',Getdate()),
+
+
+
+
+
+
--- SQL QUery/20250419_TEchnology _KK.sql
+++ SQL QUery/20250419_TEchnology _KK.sql
@@ -7,7 +7,7 @@ DBCC CHECKIDENT ('KMM.Technology', RESEED, 99);
 Insert Into KMM.Technology(TItle,Isactive,Type,Created_by,Created_at)
 Values
 
----Front-end 
+---Front-end   ---Skill done
 ('Angular',1,'Front-End','KK',GetDate()),
 ('React',1,'Front-End','KK',GetDate()),
 ('Vue.js',1,'Front-End','KK',GetDate()),
@@ -17,10 +17,11 @@ Values
 
 ('Node.js',1,'Back-End','KK',GetDate()),
 ('.Net',1,'Back-End','KK',GetDate()),
-('Go',1,'Back-End','KK',GetDate()),
-('Rust',1,'Back-End','KK',GetDate()),
 
----DB
+--('Go',1,'Back-End','KK',GetDate()),
+--('Rust',1,'Back-End','KK',GetDate()),
+
+---DB      ---Skill done
 
 ('SQL',1,'DB','KK',GetDate()),
 ('PostgreSQL',1,'DB','KK',GetDate()),
